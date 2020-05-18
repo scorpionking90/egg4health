48f8116a3b2fa5e4b513698d4b2d079dde040bf4
S,Kiran Kumar <Kiran.S@Cerner.com>
2020-05-18 11:44:05 +0530

    commiting 2

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-
-
-
-
 /* Theme variables */
 import './theme/variables.css';
 
